d35a88fb4287da254916894a88ec6b387eac79d6
NasreddinHodja <tbizetde@gmail.com>
2021-02-06 12:01:50 -0300

    format

--- public/scripts/GameLoop.js
+++ public/scripts/GameLoop.js
@@ -10,7 +10,7 @@ const ICONS = [0,1,2,3,4,5];
 
 var game = null;
 
-function getStartGame(width,height,generator){
+function getStartGame(width, height, generator){
 
     //get a initial game without repeating sequences
     let game = new GameImpl(width, height, generator);
@@ -22,44 +22,43 @@ function getStartGame(width,height,generator){
     console.log(game.toString());
 
     return game;
-
 }
 
 function getMove(){
-    var firstPosition = document.getElementById("first").value.split(",").map(el => parseInt(el));
-    var secondPosition = document.getElementById("second").value.split(",").map(el => parseInt(el));
+    var firstPosition = document.getElementById("first")
+        .value.split(",").map(el => parseInt(el));
+    var secondPosition = document.getElementById("second")
+        .value.split(",").map(el => parseInt(el));
 
     return [firstPosition,secondPosition];
-
-
 }
 
 function makeMove(){
     let move = getMove();
-    let cell1 = new Cell(move[0][0],move[0][1],game.getIcon(move[0][0],move[0][1]));
-    let cell2 = new Cell(move[1][0],move[1][1],game.getIcon(move[1][0],move[1][1]));
+    let cell1 = new Cell(move[0][0], move[0][1],
+                         game.getIcon(move[0][0], move[0][1]));
+    let cell2 = new Cell(move[1][0], move[1][1],
+                         game.getIcon(move[1][0], move[1][1]));
 
     if(game.select([cell1,cell2])){
 
         game.removeAllRuns();
-        for(let i=0;i<WIDTH;i++){
+        for(let i=0; i < WIDTH; i++){
             game.fillCollumn(i);
         }
-
     }else{
         console.log("Invalid Move");
     }
 
     console.log(game.toString());
     console.log("Score: ",game.score);
-
 }
 
 
 
 function startGame(){
     let generator = new BasicIconGenerator(ICONS);
-    game = getStartGame(WIDTH,HEIGHT,generator);
+    game = getStartGame(WIDTH, HEIGHT, generator);
 }
 
 startGame();
